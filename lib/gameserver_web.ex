41de4e1146887f85ce1106a6fe29c0615b2658c0
Ellyse <ellyse@ellyxir.com>
2026-02-27 19:44:12 +0000

    fix initial credo issues

--- lib/gameserver_web.ex
+++ lib/gameserver_web.ex
@@ -88,8 +88,8 @@ defmodule GameserverWeb do
       import GameserverWeb.CoreComponents
 
       # Common modules used in templates
-      alias Phoenix.LiveView.JS
       alias GameserverWeb.Layouts
+      alias Phoenix.LiveView.JS
 
       # Routes generation with the ~p sigil
       unquote(verified_routes())
