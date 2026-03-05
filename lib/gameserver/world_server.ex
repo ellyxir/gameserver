53ef5136c44aff604ffad30088d9b7d22d0da685
Ellyse <ellyse@ellyxir.com>
2026-03-05 19:17:41 +0000

    add join and leave to worldserver

--- lib/gameserver/world_server.ex
+++ lib/gameserver/world_server.ex
@@ -1,21 +1,68 @@
 defmodule Gameserver.WorldServer do
   @moduledoc """
-
-  A named GenServer that tracks users' presence and location
+  A named GenServer that tracks which users' presence and location in the world.
   """
 
   use GenServer
 
+  alias Gameserver.User
+
+  # State: %__MODULE__{users: %{Ecto.UUID.t() => User.t()}}
+  defstruct users: %{}
+
+  @typedoc "Error reasons for join/leave operations"
+  @type error_reason() :: :already_joined | :not_found
+
+  # Client API
+
   @doc """
-  Starts the WorldServer and registers it under its module name.
+  Starts the WorldServer. Accepts `:name` option, defaults to module name.
+  During unit tests we start up without the name so that we can reset.
   """
   @spec start_link(keyword()) :: GenServer.on_start()
   def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+    name = Keyword.get(opts, :name, __MODULE__)
+    GenServer.start_link(__MODULE__, opts, name: name)
   end
+
+  @doc """
+  Adds a user to the world.
+  """
+  @spec join(User.t(), GenServer.server()) :: :ok | {:error, error_reason()}
+  def join(%User{} = user, server \\ __MODULE__) do
+    GenServer.call(server, {:join, user})
+  end
+
+  @doc """
+  Removes a user from the world.
+  """
+  @spec leave(User.t(), GenServer.server()) :: :ok | {:error, error_reason()}
+  def leave(%User{} = user, server \\ __MODULE__) do
+    GenServer.call(server, {:leave, user})
+  end
+
+  # Server callbacks
 
   @impl GenServer
   def init(_opts) do
-    {:ok, %{}}
+    {:ok, %__MODULE__{}}
+  end
+
+  @impl GenServer
+  def handle_call({:join, %User{id: id} = user}, _from, %__MODULE__{users: users} = state) do
+    if Map.has_key?(users, id) do
+      {:reply, {:error, :already_joined}, state}
+    else
+      {:reply, :ok, %{state | users: Map.put(users, id, user)}}
+    end
+  end
+
+  @impl GenServer
+  def handle_call({:leave, %User{id: id}}, _from, %__MODULE__{users: users} = state) do
+    if Map.has_key?(users, id) do
+      {:reply, :ok, %{state | users: Map.delete(users, id)}}
+    else
+      {:reply, {:error, :not_found}, state}
+    end
   end
 end
--- test/gameserver/world_server_test.exs
+++ test/gameserver/world_server_test.exs
@@ -1,11 +1,55 @@
 defmodule Gameserver.WorldServerTest do
-  use ExUnit.Case, async: false
+  use ExUnit.Case, async: true
 
+  alias Gameserver.User
   alias Gameserver.WorldServer
+
+  setup do
+    pid = start_supervised!({WorldServer, name: nil})
+    {:ok, server: pid}
+  end
 
   describe "genserver lifecycle" do
     test "is started and registered by application" do
       assert Process.whereis(WorldServer) != nil
+    end
+  end
+
+  describe "join/2" do
+    test "adds user to the world", %{server: server} do
+      {:ok, user} = User.new("alice")
+
+      assert :ok = WorldServer.join(user, server)
+    end
+
+    test "returns error when user already joined", %{server: server} do
+      {:ok, user} = User.new("alice")
+
+      :ok = WorldServer.join(user, server)
+      assert {:error, :already_joined} = WorldServer.join(user, server)
+    end
+
+    test "allows rejoin after leaving", %{server: server} do
+      {:ok, user} = User.new("alice")
+
+      :ok = WorldServer.join(user, server)
+      :ok = WorldServer.leave(user, server)
+      assert :ok = WorldServer.join(user, server)
+    end
+  end
+
+  describe "leave/2" do
+    test "removes user from the world", %{server: server} do
+      {:ok, user} = User.new("alice")
+      :ok = WorldServer.join(user, server)
+
+      assert :ok = WorldServer.leave(user, server)
+    end
+
+    test "returns error when user not in world", %{server: server} do
+      {:ok, user} = User.new("alice")
+
+      assert {:error, :not_found} = WorldServer.leave(user, server)
     end
   end
 end
