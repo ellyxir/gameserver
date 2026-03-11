d202714ea2584e7ab51c2114c49dc791c574e24b
Ellyse <ellyse@ellyxir.com>
2026-03-11 18:08:04 +0000

    use coord() tuples in map public api instead of separate x, y args

--- lib/gameserver/map.ex
+++ lib/gameserver/map.ex
@@ -9,10 +9,13 @@ defmodule Gameserver.Map do
   defstruct [:width, :height, :tiles]
 
   @typedoc "Tile types that can occupy a map cell"
-  @type tile() :: :wall | :floor | :door
+  @type tile() :: :wall | :floor | :door | :upstairs | :downstairs
 
   @typedoc "An {x, y} coordinate pair"
   @type coord() :: {non_neg_integer(), non_neg_integer()}
+
+  @typedoc "A room is a rectangle. coord() for top right point, and w,h for width and height"
+  @type room() :: {coord(), non_neg_integer(), non_neg_integer()}
 
   @typedoc "A 2D grid map with configurable dimensions"
   @type t() :: %__MODULE__{
@@ -44,9 +47,9 @@ defmodule Gameserver.Map do
   @doc """
   Returns the tile at the given coordinates.
   """
-  @spec get_tile(t(), integer(), integer()) :: {:ok, tile()} | {:error, :out_of_bounds}
-  def get_tile(%__MODULE__{} = map, x, y) do
-    if in_bounds?(map, x, y) do
+  @spec get_tile(t(), coord()) :: {:ok, tile()} | {:error, :out_of_bounds}
+  def get_tile(%__MODULE__{} = map, {x, y} = coord) do
+    if in_bounds?(map, coord) do
       {:ok, Map.get(map.tiles, {x, y})}
     else
       {:error, :out_of_bounds}
@@ -56,9 +59,9 @@ defmodule Gameserver.Map do
   @doc """
   Returns the tile at the given coordinates, raises if out of bounds.
   """
-  @spec get_tile!(t(), integer(), integer()) :: tile()
-  def get_tile!(%__MODULE__{} = map, x, y) do
-    case get_tile(map, x, y) do
+  @spec get_tile!(t(), coord()) :: tile()
+  def get_tile!(%__MODULE__{} = map, {x, y} = coord) do
+    case get_tile(map, coord) do
       {:ok, tile} -> tile
       {:error, :out_of_bounds} -> raise ArgumentError, "coordinates (#{x}, #{y}) out of bounds"
     end
@@ -69,10 +72,10 @@ defmodule Gameserver.Map do
 
   Returns the map unchanged if coordinates are out of bounds.
   """
-  @spec set_tile(t(), integer(), integer(), tile()) :: t()
-  def set_tile(%__MODULE__{} = map, x, y, tile) do
-    if in_bounds?(map, x, y) do
-      %{map | tiles: Map.put(map.tiles, {x, y}, tile)}
+  @spec set_tile(t(), coord(), tile()) :: t()
+  def set_tile(%__MODULE__{} = map, {_x, _y} = coord, tile) do
+    if in_bounds?(map, coord) do
+      %{map | tiles: Map.put(map.tiles, coord, tile)}
     else
       map
     end
@@ -81,21 +84,21 @@ defmodule Gameserver.Map do
   @doc """
   Returns true if the coordinates are within the map bounds.
   """
-  @spec in_bounds?(t(), integer(), integer()) :: boolean()
-  def in_bounds?(%__MODULE__{width: width, height: height}, x, y) do
+  @spec in_bounds?(t(), coord()) :: boolean()
+  def in_bounds?(%__MODULE__{width: width, height: height}, {x, y}) do
     x >= 0 and x < width and y >= 0 and y < height
   end
 
   @doc """
   Fills a rectangular area with the given tile type.
 
-  The rectangle starts at `(x, y)` and extends `w` tiles wide and `h` tiles tall.
+  The rectangle starts at `{x, y}` and extends `w` tiles wide and `h` tiles tall.
   """
-  @spec fill_rect(t(), integer(), integer(), pos_integer(), pos_integer(), tile()) :: t()
-  def fill_rect(%__MODULE__{} = map, x, y, w, h, tile) do
+  @spec fill_rect(t(), room(), tile()) :: t()
+  def fill_rect(%__MODULE__{} = map, {{x, y}, w, h}, tile) do
     Enum.reduce(x..(x + w - 1), map, fn cx, acc ->
       Enum.reduce(y..(y + h - 1), acc, fn cy, acc2 ->
-        set_tile(acc2, cx, cy, tile)
+        set_tile(acc2, {cx, cy}, tile)
       end)
     end)
   end
@@ -112,20 +115,20 @@ defmodule Gameserver.Map do
 
     new(width, height)
     # Room 1: top-left (4x4 at position 1,1)
-    |> fill_rect(1, 1, 4, 4, :floor)
+    |> fill_rect({{1, 1}, 4, 4}, :floor)
     # Room 2: top-right (4x4 at position 10,1)
-    |> fill_rect(10, 1, 4, 4, :floor)
+    |> fill_rect({{10, 1}, 4, 4}, :floor)
     # Room 3: bottom-center (5x4 at position 5,10)
-    |> fill_rect(5, 10, 5, 4, :floor)
+    |> fill_rect({{5, 10}, 5, 4}, :floor)
     # Corridor from room 1 to room 2 (horizontal at y=2)
-    |> fill_rect(5, 2, 5, 1, :floor)
+    |> fill_rect({{5, 2}, 5, 1}, :floor)
     # Corridor from room 1 down to room 3 (vertical at x=3)
-    |> fill_rect(3, 5, 1, 5, :floor)
+    |> fill_rect({{3, 5}, 1, 5}, :floor)
     # Corridor from room 2 down to room 3 (vertical at x=11)
-    |> fill_rect(11, 5, 1, 5, :floor)
+    |> fill_rect({{11, 5}, 1, 5}, :floor)
     # Connect vertical corridors to room 3 (horizontal at y=10)
-    |> fill_rect(3, 10, 3, 1, :floor)
-    |> fill_rect(10, 10, 2, 1, :floor)
+    |> fill_rect({{3, 10}, 3, 1}, :floor)
+    |> fill_rect({{10, 10}, 2, 1}, :floor)
   end
 
   @doc """
@@ -137,7 +140,7 @@ defmodule Gameserver.Map do
   def to_ascii(%__MODULE__{width: width, height: height} = map) do
     for y <- 0..(height - 1) do
       for x <- 0..(width - 1), into: "" do
-        tile_to_char(get_tile!(map, x, y))
+        tile_to_char(get_tile!(map, {x, y}))
       end
     end
   end
