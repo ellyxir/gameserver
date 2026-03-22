e4bcfcdb2071a04ed4c9cd014811bb18aa0583ef
Ellyse <ellyse@ellyxir.com>
2026-03-22 22:54:36 +0000

    add update_entity, list_entities, and pubsub broadcasts

--- lib/gameserver/entity_server.ex
+++ lib/gameserver/entity_server.ex
@@ -7,6 +7,14 @@ defmodule Gameserver.EntityServer do
   alias Gameserver.Entity
   alias Gameserver.UUID
 
+  @entity_topic "entity:changes"
+
+  @doc """
+  Returns the PubSub topic for entity change broadcasts.
+  """
+  @spec entity_topic() :: String.t()
+  def entity_topic, do: @entity_topic
+
   @typedoc """
   map of entity uuids to entities
   """
@@ -51,9 +59,31 @@ defmodule Gameserver.EntityServer do
     GenServer.call(server, {:remove_entity, id})
   end
 
+  @doc """
+  Applies an update function to an entity. Returns `{:ok, updated_entity}` or
+  `{:error, :not_found}`.
+  """
+  @spec update_entity(UUID.t(), (Entity.t() -> Entity.t()), GenServer.server()) ::
+          {:ok, Entity.t()} | {:error, :not_found | {:update_failed, term()}}
+  def update_entity(id, fun, server \\ __MODULE__) when is_binary(id) and is_function(fun, 1) do
+    GenServer.call(server, {:update_entity, id, fun})
+  end
+
+  @doc """
+  Returns all entities.
+  """
+  @spec list_entities(GenServer.server()) :: [Entity.t()]
+  def list_entities(server \\ __MODULE__) do
+    GenServer.call(server, :list_entities)
+  end
+
   # Server callbacks
 
   @impl GenServer
+  def handle_call(:list_entities, _from, state) do
+    {:reply, Map.values(state), state}
+  end
+
   def handle_call({:get_entity, id}, _from, state) do
     case Map.get(state, id) do
       nil -> {:reply, {:error, :not_found}, state}
@@ -61,8 +91,26 @@ defmodule Gameserver.EntityServer do
     end
   end
 
+  def handle_call({:update_entity, id, fun}, _from, state) do
+    case Map.get(state, id) do
+      nil ->
+        {:reply, {:error, :not_found}, state}
+
+      entity ->
+        case safe_apply(fun, [entity]) do
+          {:ok, updated} ->
+            broadcast({:entity_updated, updated})
+            {:reply, {:ok, updated}, Map.put(state, id, updated)}
+
+          {:error, reason} ->
+            {:reply, {:error, {:update_failed, reason}}, state}
+        end
+    end
+  end
+
   def handle_call({:remove_entity, id}, _from, state) do
     if Map.has_key?(state, id) do
+      broadcast({:entity_removed, id})
       {:reply, :ok, Map.delete(state, id)}
     else
       {:reply, {:error, :not_found}, state}
@@ -73,7 +121,21 @@ defmodule Gameserver.EntityServer do
     if Map.has_key?(state, id) do
       {:reply, {:error, :already_exists}, state}
     else
+      broadcast({:entity_created, entity})
       {:reply, :ok, Map.put(state, id, entity)}
     end
+  end
+
+  @spec safe_apply(function(), [term()]) :: {:ok, term()} | {:error, term()}
+  defp safe_apply(fun, args) do
+    {:ok, apply(fun, args)}
+  rescue
+    e -> {:error, Exception.message(e)}
+  catch
+    :exit, reason -> {:error, {:exit, reason}}
+  end
+
+  defp broadcast(message) do
+    Phoenix.PubSub.broadcast(Gameserver.PubSub, @entity_topic, message)
   end
 end
--- test/gameserver/entity_server_test.exs
+++ test/gameserver/entity_server_test.exs
@@ -58,4 +58,96 @@ defmodule Gameserver.EntityServerTest do
       assert {:error, :not_found} = EntityServer.remove_entity(UUID.generate(), server)
     end
   end
+
+  describe "update_entity/3" do
+    test "applies update function and returns updated entity", %{server: server} do
+      entity = create_entity!(server, pos: {3, 3})
+
+      assert {:ok, updated} =
+               EntityServer.update_entity(entity.id, &%{&1 | pos: {4, 3}}, server)
+
+      assert updated.pos == {4, 3}
+      assert {:ok, ^updated} = EntityServer.get_entity(entity.id, server)
+    end
+
+    test "returns error for unknown id", %{server: server} do
+      assert {:error, :not_found} =
+               EntityServer.update_entity(UUID.generate(), &%{&1 | pos: {0, 0}}, server)
+    end
+
+    test "returns error and preserves state when update function raises", %{server: server} do
+      entity = create_entity!(server, pos: {3, 3})
+
+      assert {:error, {:update_failed, _reason}} =
+               EntityServer.update_entity(entity.id, fn _e -> raise "boom" end, server)
+
+      assert {:ok, ^entity} = EntityServer.get_entity(entity.id, server)
+    end
+  end
+
+  describe "list_entities/1" do
+    test "returns all entities", %{server: server} do
+      goblin = create_entity!(server, name: "goblin")
+      rat = create_entity!(server, name: "rat", pos: {5, 5})
+
+      entities = EntityServer.list_entities(server)
+      assert length(entities) == 2
+      assert goblin in entities
+      assert rat in entities
+    end
+
+    test "returns empty list when no entities", %{server: server} do
+      assert [] = EntityServer.list_entities(server)
+    end
+  end
+
+  describe "pubsub broadcasts" do
+    setup %{server: server} do
+      Phoenix.PubSub.subscribe(Gameserver.PubSub, EntityServer.entity_topic())
+      %{server: server}
+    end
+
+    test "broadcasts on create_entity", %{server: server} do
+      entity = Entity.new(name: "goblin", type: :mob, pos: {3, 3})
+      :ok = EntityServer.create_entity(entity, server)
+
+      assert_receive {:entity_created, ^entity}
+    end
+
+    test "does not broadcast on duplicate create", %{server: server} do
+      entity = create_entity!(server)
+      assert_receive {:entity_created, _}
+
+      {:error, :already_exists} = EntityServer.create_entity(entity, server)
+      refute_receive {:entity_created, _}
+    end
+
+    test "broadcasts on remove_entity", %{server: server} do
+      entity = create_entity!(server)
+      assert_receive {:entity_created, _}
+
+      :ok = EntityServer.remove_entity(entity.id, server)
+      assert_receive {:entity_removed, id} when id == entity.id
+    end
+
+    test "does not broadcast on failed remove", %{server: server} do
+      {:error, :not_found} = EntityServer.remove_entity(UUID.generate(), server)
+      refute_receive {:entity_removed, _}
+    end
+
+    test "broadcasts on update_entity", %{server: server} do
+      entity = create_entity!(server, pos: {3, 3})
+      assert_receive {:entity_created, _}
+
+      {:ok, updated} = EntityServer.update_entity(entity.id, &%{&1 | pos: {4, 3}}, server)
+      assert_receive {:entity_updated, ^updated}
+    end
+
+    test "does not broadcast on failed update", %{server: server} do
+      {:error, :not_found} =
+        EntityServer.update_entity(UUID.generate(), &%{&1 | pos: {0, 0}}, server)
+
+      refute_receive {:entity_updated, _}
+    end
+  end
 end
