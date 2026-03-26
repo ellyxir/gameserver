05f416898d7242a8394aa10b9b1e0ee8cd2197b7
Ellyse <ellyse@ellyxir.com>
2026-03-26 14:47:34 +0000

    add auto-scroll hook to combat log

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,12 +27,13 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import {hooks as colocatedHooks} from "phoenix-colocated/gameserver"
 import topbar from "../vendor/topbar"
+import ScrollBottom from "./scroll_bottom"
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 const liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   params: {_csrf_token: csrfToken},
-  hooks: {...colocatedHooks},
+  hooks: {...colocatedHooks, ScrollBottom},
 })
 
 // Show progress bar on live navigation and form submits
