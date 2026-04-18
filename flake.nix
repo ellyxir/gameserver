7197f052edcdaa0cff0059df27955afb46682b23
Ellyse <ellyse@ellyxir.com>
2026-04-18 18:19:15 +0000

    flake for elixir

---
+++ flake.nix
@@ -0,0 +1,25 @@
+# `nix develop` to start shell
+# `nix flake update` to update all packages (inputs) 
+{
+  description = "elixir dev flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          beam28Packages.elixir_1_20
+          beam28Packages.elixir-ls
+        ];
+      };
+    };
+}
